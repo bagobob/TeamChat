72d7b981572152269c3983c21670055e60289689
nisrine-moussaoui <72378095+nisrine-moussaoui@users.noreply.github.com>
2020-11-05 14:47:30 +0100

    Update User.php

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -167,4 +167,9 @@ class User implements UserInterface
 
         return $this;
     }
+
+    public function getFullName(): ?string
+    {
+        return $this->getFirstName(). ' ' . $this->getLastName();
+    }
 }
