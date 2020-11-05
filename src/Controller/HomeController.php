ff44cf4be4ab07ec89b51b00ca95b6873cd8e8dc
dijabakkali <72412841+dijabakkali@users.noreply.github.com>
2020-11-05 16:14:34 +0100

    Update HomeController.php

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,10 +2,15 @@
 
 namespace App\Controller;
 
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class HomeController extends AbstractController
 {
@@ -70,4 +75,36 @@ class HomeController extends AbstractController
             'users'    => $users,
         ]);
     }
+    
+    /**
+ * @Route("/profil/edit", name="update_profil")
+ */
+public function update_pass(Request $request,UserPasswordEncoderInterface $passwordEncoder)
+{
+    if (!($this->getUser())) {
+        $this->addFlash('error', 'You must logged in');
+
+        return $this->redirectToRoute('app_login');
+    }
+    $entityManager = $this->getDoctrine()->getManager();
+    dump($request);
+    $old_pwd = $request->get('old_password'); 
+    $new_pwd = $request->get('new_password'); 
+    $new_pwd_confirm = $request->get('new_password_confirm');
+    $user = $this->getUser();
+    $checkPass = $passwordEncoder->isPasswordValid($user, $old_pwd);
+    if($checkPass === true) {
+        $new_pwd_encoded = $passwordEncoder->encodePassword($user, $new_pwd_confirm);  
+        $user->setPassword($new_pwd_encoded); 
+        $entityManager->flush();
+    } else {
+        $this->addFlash('error', 'Mot de passe incorrect ');
+
+        return $this->redirectToRoute('update_profil');
+    }
+    return $this->render('home/profile.html.twig', [
+        'controller_name' => 'HomeController',
+    ]);
+    
+}
 }
