4dc6c28ae80b72b39de5baf006dae3d787970f63
Boris Tibago <49928514+bagobob@users.noreply.github.com>
2020-11-11 03:43:01 +0100

    Modification agenda et annuaire

---
+++ src/Entity/Agenda.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Entity;
+
+use App\Entity\Traits\Timestampable;
+use App\Repository\AgendaRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity(repositoryClass=AgendaRepository::class)
+ * @ORM\HasLifecycleCallbacks
+ */
+class Agenda
+{
+    use Timestampable;
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank(message="Please enter your first name")
+     */
+    private $firstName;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank(message="Please enter your last name")
+     */
+    private $lastName;
+
+    /**
+     * @ORM\Column(type="string", length=180)
+     * @Assert\NotBlank(message="Please enter your username")
+     */
+    private $username;
+
+
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFirstName(): ?string
+    {
+        return $this->firstName;
+    }
+
+    public function setFirstName(string $firstName): self
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    public function getLastName(): ?string
+    {
+        return $this->lastName;
+    }
+
+    public function setLastName(string $lastName): self
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUsername(): string
+    {
+        return (string) $this->username;
+    }
+
+    public function setUsername(string $username): self
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+}
---
+++ src/Form/UserFormType.php
@@ -0,0 +1,38 @@
+<?php
+
+
+namespace App\Form;
+
+
+use App\Entity\Agenda;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('firstName')
+            ->add('lastName')
+            ->add('userName')
+            ->add('createdAt', DateTimeType::class, [
+                'data' => new \DateTime(),
+            ])
+            ->add('updatedAt', DateTimeType::class,[
+                'data' => new \DateTime(),
+            ])
+            ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Agenda::class,
+        ]);
+    }
+
+
+}
